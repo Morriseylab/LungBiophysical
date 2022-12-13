61c36ccfc20547377b6a882ef04afde1941004e5
Morriseylab <43073258+Morriseylab@users.noreply.github.com>
2022-12-13 14:05:14 -0500

    Update runSTARsolo.sh

--- runSTARsolo.sh
+++ runSTARsolo.sh
@@ -5,8 +5,8 @@
 --soloUMIfiltering MultiGeneUMI \
 --soloCBmatchWLtype 1MM_multi_pseudocounts \
 --soloCBwhitelist ~/NGSshare/3M-february-2018.txt \
---readFilesIn ~/NGS/30-624603005/00_fastq/EEM-scRNA-113_S1_L001_R2_001.fastq.gz ~/NGS/30-624603005/00_fastq/EEM-scRNA-113_S1_L001_R1_001.fastq.gz \
---genomeDir ~/NGSshare/mm39_tdTomato_STAR \
+--readFilesIn R2.fastq.gz R1.fastq.gz \
+--genomeDir ~/NGSshare/mm39_STAR \
 --runThreadN 64 \
 --readFilesCommand zcat \
 --soloBarcodeReadLength 0 \
